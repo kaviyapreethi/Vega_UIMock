bc1641eecf5224fb58b34c1fe4d224597ab0bcda
kaviyapreethi <preethikaviya97@gmail.com>
2019-10-01 18:37:51 +0530

    scroll screen

--- js/app.js
+++ js/app.js
@@ -28,3 +28,59 @@ function activeFunc() {
     });
   }
 }
+
+//on scroll active
+function myFn() {
+  {
+    console.log(1);
+    var section = document.querySelectorAll(".scrolling");
+    var sections = {};
+    var i = 0;
+
+    Array.prototype.forEach.call(section, function(e) {
+      sections[e.id] = e.offsetTop;
+    });
+
+    window.onscroll = function() {
+      var scrollPosition =
+        document.documentElement.scrollTop || document.body.scrollTop;
+
+      for (i in sections) {
+        if (sections[i] <= scrollPosition) {
+          document.querySelector(".active").setAttribute("class", " ");
+          document
+            .querySelector("a[href*=" + i + "]")
+            .setAttribute("class", "active");
+        }
+      }
+    };
+  }
+}
+
+//on scroll active change it
+
+/*function myFn() {
+  var header = document.getElementById("scrollme");
+  var btns = header.getElementsByClassName("scrolling");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("mouseover", function() {
+      var current = document.getElementsByClassName("active");
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "");
+      }
+      this.className += " active";
+    });
+  }
+}*/
+
+/*var a = document.getElementById("myDropdown");
+var b = a.getElementsByClassName("btn");
+
+for (var i = 0; i < b.length; i++) {
+  b[i] = document.getElementsByClassName("btn");
+  this.className += " active";
+}
+var a = document.getElementById("myDropdown");
+var b = a.getElementsByClassName("btn");
+for (var i = 0; i < b.length; i++) {
+  b.className += " active";*/
